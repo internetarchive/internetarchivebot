1c66a825510bc0ad59b9b339bf75311d907a6211
cyberpower678 <cybernet678@yahoo.com>
2018-05-30 00:51:48 -0400

    Commit part 4
    Deleting more files

--- Parser/frwiki.php
+++ Parser/frwiki.php
@@ -36,6 +36,7 @@
  */
 class frwikiParser extends Parser {
 
+	//FIXME: The master no longer considers wikiwix invalid.  Remove child class after merging
 	/**
 	 * Parses a given refernce/external link string and returns details about it.
 	 *
@@ -56,33 +57,6 @@ class frwikiParser extends Parser {
 		}
 
 		return $returnArray;
-	}
-
-	/**
-	 * Get page date formatting standard
-	 *
-	 * @param bool|string $default Return default format, or return supplied date format of timestamp, provided a page
-	 *     tag doesn't override it.
-	 *
-	 * @access protected
-	 * @abstract
-	 * @author Maximilian Doerr (Cyberpower678)
-	 * @license https://www.gnu.org/licenses/gpl.txt
-	 * @copyright Copyright (c) 2015-2017, Maximilian Doerr
-	 * @return string Format to be fed in time()
-	 */
-	protected function retrieveDateFormat( $default = false ) {
-		if( !is_bool( $default ) &&
-		    preg_match( '/\d\d? (?:January|janvier|February|février|March|mars|April|avril|May|mai|June|juin|July|juillet|August|août|September|septembre|October|octobre|November|novembre|December|décembre) \d{4}/i',
-		                $default
-		    )
-		) return '%-e %B %Y';
-		elseif( !is_bool( $default ) &&
-		        preg_match( '/(?:January|janvier|February|février|March|mars|April|avril|May|mai|June|juin|July|juillet|August|août|September|septembre|October|octobre|November|novembre|December|décembre) \d\d?\, \d{4}/i',
-		                    $default
-		        )
-		) return '%B %-e, %Y';
-		else return '%-e %B %Y';
 	}
 
 	/**
@@ -252,44 +226,6 @@ class frwikiParser extends Parser {
 	}
 
 	/**
-	 * Generates an appropriate archive template if it can.
-	 *
-	 * @access protected
-	 * @abstract
-	 * @author Maximilian Doerr (Cyberpower678)
-	 * @license https://www.gnu.org/licenses/gpl.txt
-	 * @copyright Copyright (c) 2015-2017, Maximilian Doerr
-	 *
-	 * @param $link Current link being modified
-	 * @param $temp Current temp result from fetchResponse
-	 *
-	 * @return bool If successful or not
-	 */
-	protected function generateNewArchiveTemplate( &$link, &$temp ) {
-		//We need the archive host, to pick the right template.
-		if( !isset( $link['newdata']['archive_host'] ) ) $link['newdata']['archive_host'] =
-			$this->getArchiveHost( $temp['archive_url'] );
-		//If the archive template is being used improperly, delete the parameters, and start fresh.
-		if( $link['has_archive'] === true &&
-		    $link['archive_type'] == "invalid"
-		) unset( $link['archive_template']['parameters'] );
-		switch( $link['newdata']['archive_host'] ) {
-			case "wayback":
-				$link['newdata']['archive_template']['name'] = "Lien archive";
-				$link['newdata']['archive_template']['parameters']['url'] = $link['url'];
-				if( $temp['archive_time'] != 0 ) $link['newdata']['archive_template']['parameters']['date'] =
-					date( 'YmdHis', $temp['archive_time'] );
-				else $link['newdata']['archive_template']['parameters']['date'] = "*";
-				$link['newdata']['archive_template']['parameters']['titre'] = "Copie archivée";
-				break;
-			default:
-				return false;
-		}
-
-		return true;
-	}
-
-	/**
 	 * Modify link that can't be rescued
 	 *
 	 * @param array $link Link being analyzed
@@ -318,112 +254,6 @@ class frwikiParser extends Parser {
 			$link['newdata']['tag_template']['name'] = "lien brisé";
 			$link['newdata']['tag_template']['parameters']['url'] = $link['url'];
 			$link['newdata']['tag_template']['parameters']['titre'] = $title;
-		}
-	}
-
-	/**
-	 * Analyze the remainder string
-	 *
-	 * @param array $returnArray Array being generated in master function
-	 * @param string $remainder Remainder string
-	 *
-	 * @access protected
-	 * @abstract
-	 * @author Maximilian Doerr (Cyberpower678)
-	 * @license https://www.gnu.org/licenses/gpl.txt
-	 * @copyright Copyright (c) 2015-2017, Maximilian Doerr
-	 * @return void
-	 */
-	protected function analyzeRemainder( &$returnArray, &$remainder ) {
-
-		if( preg_match( $this->fetchTemplateRegex( $this->commObject->config['archive_tags'] ), $remainder, $params2
-		) ) {
-			$returnArray['archive_type'] = "template";
-			$returnArray['archive_template'] = [];
-			$returnArray['archive_template']['parameters'] = $this->getTemplateParameters( $params2[2] );
-			$returnArray['archive_template']['name'] = str_replace( "{{", "", $params2[1] );
-			$returnArray['archive_template']['string'] = $params2[0];
-
-			$returnArray['has_archive'] = true;
-
-			//If there is a wayback tag present, process it
-			if( preg_match( $this->fetchTemplateRegex( $this->commObject->config['archive1_tags'] ), $remainder,
-			                $params2
-			) ) {
-				$returnArray['archive_host'] = "wayback";
-
-				//Look for the URL.  If there isn't any found, the template is being used wrong.
-				if( isset( $returnArray['archive_template']['parameters']['url'] ) ) {
-					$url =
-						htmlspecialchars_decode( $this->filterText( $returnArray['archive_template']['parameters']['url'],
-						                                            true
-						)
-						);
-				} elseif( isset( $returnArray['archive_template']['parameters'][1] ) ) {
-					$url =
-						htmlspecialchars_decode( $this->filterText( $returnArray['archive_template']['parameters'][1],
-						                                            true
-						)
-						);
-				} elseif( isset( $returnArray['archive_template']['parameters']['site'] ) ) {
-					$url =
-						htmlspecialchars_decode( $this->filterText( $returnArray['archive_template']['parameters']['site'],
-						                                            true
-						)
-						);
-				} else {
-					$returnArray['archive_url'] = "x";
-					$returnArray['archive_type'] = "invalid";
-				}
-
-				//Look for archive timestamp.  If there isn't any, then it's not pointing a snapshot, which makes it harder for the reader and other editors.
-				if( isset( $returnArray['archive_template']['parameters']['date'] ) ) {
-					$returnArray['archive_time'] =
-						self::strtotime( $timestamp =
-							                 $this->filterText( $returnArray['archive_template']['parameters']['date'],
-							                                    true
-							                 )
-						);
-					$returnArray['archive_url'] =
-						"https://web.archive.org/web/$timestamp/$url";
-				} else {
-					$returnArray['archive_time'] = "x";
-					$returnArray['archive_url'] = "https://web.archive.org/web/*/$url";
-					$returnArray['archive_type'] = "invalid";
-				}
-
-				//If the original URL isn't present, then we are dealing with a stray archive template.
-				if( !isset( $returnArray['url'] ) ) {
-					$returnArray['archive_type'] = "invalid";
-					$returnArray['url'] = $url;
-					$returnArray['link_type'] = "stray";
-					$returnArray['is_archive'] = true;
-				}
-
-				//Check for a malformation or template misuse.  The URL field needs the original URL, not the archive URL.
-				if( $returnArray['archive_url'] == "x" || strpos( $url, "archive.org" ) !== false ) {
-					if( preg_match( '/archive\.org\/(web\/)?(\d*?|\*)\/(\S*)\s?/i', $url, $params3 ) ) {
-						$returnArray['archive_type'] = "invalid";
-						if( $params3[2] != "*" ) $returnArray['archive_time'] = self::strtotime( $params3[2] );
-						else $returnArray['archive_time'] = "x";
-						$returnArray['archive_url'] = "https://web." . $this->filterText( $params3[0], true );
-					} else {
-						$returnArray['archive_type'] = "invalid";
-					}
-				}
-			}
-		}
-
-		if( preg_match( $this->fetchTemplateRegex( $this->commObject->config['deadlink_tags'] ), $remainder, $params2
-		) ) {
-			$returnArray['tagged_dead'] = true;
-			$returnArray['tag_type'] = "template";
-			if( isset( $params2[2] ) ) $returnArray['tag_template']['parameters'] =
-				$this->getTemplateParameters( $params2[2] );
-			else $returnArray['tag_template']['parameters'] = [];
-			//Flag those that can't be fixed.
-			$returnArray['tag_template']['name'] = str_replace( "{{", "", $params2[1] );
-			$returnArray['tag_template']['string'] = $params2[0];
 		}
 	}
 }
