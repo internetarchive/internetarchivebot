0df9c7fd4137e7e7ea7f73ffaa94f39c759aa209
cyberpower678 <cybernet678@yahoo.com>
2018-07-15 18:11:28 -0400

    oops
    oops

--- deadlink.php
+++ deadlink.php
@@ -23,8 +23,6 @@ if( !empty( $argv[2] ) ) {
 	define( 'UNIQUEID', $argv[2] );
 	if( UNIQUEID == "dead" ) $overrideConfig['page_scan'] = 1;
 }
-define( 'WIKIPEDIA', "huwiki" );
-$overrideConfig['page_scan'] = 1;
 
 set_include_path( get_include_path() . PATH_SEPARATOR . dirname( __FILE__ ) . DIRECTORY_SEPARATOR );
 date_default_timezone_set( "UTC" );
