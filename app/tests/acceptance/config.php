0a9d658288ad94295238949f369ebe3ad874e0c8
James Hare <jamesmhare@gmail.com>
2020-12-22 10:34:00 -0800

    Delete config.php

--- app/tests/acceptance/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// The acceptance tests use botclasses to create the test pages
-//   and get the contents after InternetArchiveBot has edited them
-// Below defines the credentials for the bot used in the acceptance tests (e.g. Community_Tech_bot)
-// Copy this file to config.php
-define( 'ACCEPTANCE_USERNAME', 'Harej' );
-define( 'ACCEPTANCE_PASSWORD', 'iabot_tests@blulueq5bbjp1ab36c7ola7iolu3l1c1' );
